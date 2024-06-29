7c93c67d3f5658abb684572bebe94de21abd334b
badasswp <badasswpdev@gmail.com>
2024-06-29 11:52:12 +0100

    chore: add plugin entry file

---
+++ search-replace-for-block-editor.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Plugin Name: Search and Replace for Block Editor
+ * Plugin URI:  https://github.com/badasswp/search-and-replace
+ * Description: Search and Replace text within the Block Editor.
+ * Version:     1.0.0
+ * Author:      badasswp
+ * Author URI:  https://github.com/badasswp
+ * License:     GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain: search-replace-for-block-editor
+ * Domain Path: /languages
+ *
+ * @package SearchReplaceForBlockEditor
+ */
+
+namespace badasswp\SearchReplaceForBlockEditor;
+
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Load Search & Replace Script for Block Editor.
+ *
+ * @since 1.0.0
+ *
+ * @wp-hook 'enqueue_block_editor_assets'
+ */
+add_action( 'enqueue_block_editor_assets', function() {
+	wp_enqueue_script(
+		'search-replace-for-block-editor',
+		plugins_url( 'search-replace-for-block-editor/dist/scripts.js' ),
+		[
+			'wp-i18n',
+			'wp-element',
+			'wp-blocks',
+			'wp-components',
+			'wp-editor',
+			'wp-hooks',
+			'wp-compose',
+			'wp-plugins',
+			'wp-edit-post'
+		],
+		'1.0.0',
+		true
+	);
+} );
