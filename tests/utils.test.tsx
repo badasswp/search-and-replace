6abf5a8e186e941a1e9bbf053322e29322335cf6
badasswp <badasswpdev@gmail.com>
2024-07-03 12:30:39 +0100

    test: add tests for getTextBlocks method

---
+++ tests/utils.test.tsx
@@ -0,0 +1,34 @@
+import { getTextBlocks } from '../src/utils';
+
+jest.mock('@wordpress/blocks', () => ({
+  getBlockTypes: jest.fn(() => {
+    return [
+      1,
+      'string',
+      true,
+      null,
+      undefined,
+      [],
+      {},
+      { name: 'core/paragraph' },
+      { category: 'text', name: 'core/paragraph' },
+      { category: 'image', name: 'core/image' },
+      { category: 'text', name: 'core/pullquote' },
+      { category: 'text', name: 'core/preformatted' },
+    ]
+  }),
+}));
+
+describe('getTextBlocks', () => {
+  it('passes and returns Blocks in the `text` category', () => {
+    const blocks = getTextBlocks();
+    expect(blocks).toEqual(
+      ['core/paragraph', 'core/pullquote', 'core/preformatted']
+    );
+  });
+
+  it('passes and returns Length of Blocks', () => {
+    const blocks = getTextBlocks();
+    expect(blocks.length).toBe(3);
+  });
+})
