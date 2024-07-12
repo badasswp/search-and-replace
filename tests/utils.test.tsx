c154577f7b4fd8dcf9e0fbe6a1d08a8fed28f710
badasswp <badasswpdev@gmail.com>
2024-07-12 21:13:19 +0100

    test: fix failing tests

--- tests/utils.test.tsx
+++ tests/utils.test.tsx
@@ -1,5 +1,4 @@
 import { getTextBlocks } from '../src/utils';
-import { getAllowedBlocks } from '../src/utils';
 
 jest.mock('@wordpress/blocks', () => ({
   getBlockTypes: jest.fn(() => {
@@ -20,10 +19,6 @@ jest.mock('@wordpress/blocks', () => ({
   }),
 }));
 
-jest.mock('@wordpress/hooks', () => ({
-  applyFilters: jest.fn(() => []),
-}));
-
 describe('getTextBlocks', () => {
   it('passes and returns Blocks in the `text` category', () => {
     const blocks = getTextBlocks();
@@ -35,29 +30,5 @@ describe('getTextBlocks', () => {
   it('passes and returns Length of Blocks', () => {
     const blocks = getTextBlocks();
     expect(blocks.length).toBe(3);
-  });
-})
-
-describe('getAllowedBlocks', () => {
-  it('passes and returns Blocks with filters applied', () => {
-    jest.mock('../src/utils', () => ({
-      getTextBlocks: jest.fn(() => {
-        return ['core/paragraph', 'core/pullquote', 'core/preformatted'];
-      }),
-    }));
-
-    const blocks = getAllowedBlocks();
-    expect(blocks).toEqual([]);
-  });
-
-  it('passes and returns Length of Blocks', () => {
-    jest.mock('../src/utils', () => ({
-      getTextBlocks: jest.fn(() => {
-        return ['core/paragraph', 'core/pullquote', 'core/preformatted'];
-      }),
-    }));
-
-    const blocks = getAllowedBlocks();
-    expect(blocks.length).toEqual(0);
   });
 })
