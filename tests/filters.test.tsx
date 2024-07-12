58ebd7989c0508e895771d02145524435a991f8c
badasswp <badasswpdev@gmail.com>
2024-07-12 23:04:50 +0100

    test: move filter test for allowedBlocks hook

--- tests/filters.test.tsx
+++ tests/filters.test.tsx
@@ -1,3 +1,66 @@
+describe('search-replace-for-block-editor.allowedBlocks', () => {
+  it('passes and returns Blocks with filters applied', () => {
+    jest.doMock('@wordpress/hooks', () => ({
+      applyFilters: jest.fn((hook: string, arg: string[]) => {
+        return [...arg, 'core/table'];
+      }),
+    }));
+
+    jest.doMock('../src/utils', () => ({
+      getTextBlocks: jest.fn(() => {
+        return ['core/paragraph', 'core/pullquote', 'core/preformatted'];
+      }),
+      getAllowedBlocks: jest.fn(() => {
+        const { applyFilters } = require('@wordpress/hooks');
+        const { getTextBlocks } = require('../src/utils');
+        return applyFilters('search-replace-for-block-editor.allowedBlocks', getTextBlocks());
+      }),
+    }));
+
+    const { getAllowedBlocks } = require('../src/utils');
+    const blocks = getAllowedBlocks();
+    expect(blocks).toEqual(
+      [
+        'core/paragraph',
+        'core/pullquote',
+        'core/preformatted',
+        'core/table',
+      ]
+    );
+    expect(blocks.length).toEqual(4);
+  });
+
+  it('passes and returns empty array of ', () => {
+    jest.doMock('@wordpress/hooks', () => ({
+      applyFilters: jest.fn((hook: string, arg: string[]) => {
+        return [];
+      }),
+    }));
+
+    jest.doMock('../src/utils', () => ({
+      getTextBlocks: jest.fn(() => {
+        return ['core/paragraph', 'core/pullquote', 'core/preformatted'];
+      }),
+      getAllowedBlocks: jest.fn(() => {
+        const { applyFilters } = require('@wordpress/hooks');
+        const { getTextBlocks } = require('../src/utils');
+        return applyFilters('search-replace-for-block-editor.allowedBlocks', getTextBlocks());
+      }),
+    }));
+
+    const { getAllowedBlocks } = require('../src/utils');
+    const blocks = getAllowedBlocks();
+    expect(blocks).toEqual([]);
+    expect(blocks.length).toEqual(0);
+  });
+
+  afterEach(() => {
+    jest.unmock('@wordpress/hooks');
+    jest.unmock('../src/utils');
+    jest.resetModules();
+  });
+});
+
 describe('search-replace-for-block-editor.keyboardShortcut', () => {
   it('passes and returns the default keyboard shortcut', () => {
     jest.doMock('@wordpress/hooks', () => ({
