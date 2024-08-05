757031aadd0cf45850aa30a58dd4b971fb5643cf
badasswp <badasswpdev@gmail.com>
2024-08-05 21:04:09 +0100

    chore: bump up plugin version

--- search-replace-for-block-editor.php
+++ search-replace-for-block-editor.php
@@ -3,7 +3,7 @@
  * Plugin Name: Search and Replace for Block Editor
  * Plugin URI:  https://github.com/badasswp/search-and-replace
  * Description: Search and Replace text within the Block Editor.
- * Version:     1.0.2
+ * Version:     1.0.3
  * Author:      badasswp
  * Author URI:  https://github.com/badasswp
  * License:     GPL v2 or later
@@ -44,7 +44,7 @@ add_action( 'enqueue_block_editor_assets', function() {
 			'wp-edit-post',
 			'wp-edit-site',
 		],
-		mt_rand(),
+		'1.0.3',
 		false,
 	);
 
