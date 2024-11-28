950dd4642ff26d56e310ebb00f9d0a12d8829f3f
badasswp <badasswpdev@gmail.com>
2024-11-28 23:00:43 +0100

    refactor: rename callback, move to bottom

--- src/app.tsx
+++ src/app.tsx
@@ -60,7 +60,7 @@ const SearchReplaceForBlockEditor = () => {
    *
    * @returns {void}
    */
-  const onSelection = () => {
+  const handleSelection = () => {
     const selectedText = window.getSelection().toString();
     const modalSelector = '.search-replace-modal';
 
@@ -68,51 +68,6 @@ const SearchReplaceForBlockEditor = () => {
       setSearchInput(selectedText);
     }
   };
-
-  /**
-   * Check if the selection is made inside target container,
-   * for e.g. the `search-replace-modal`.
-   *
-   * @since 1.2.1
-   *
-   * @param {string} selector Target selector.
-   *
-   * @returns {boolean}
-   */
-  const inContainer = (selector) => {
-    const selection = window.getSelection();
-    const targetDiv = document.querySelector(selector);
-
-    if (!selection.rangeCount || !targetDiv) {
-      return false;
-    }
-
-    const range = selection.getRangeAt(0);
-
-    return targetDiv.contains(range.startContainer) && targetDiv.contains(range.endContainer);
-  }
-
-  /**
-   * Listen for Selection.
-   *
-   * Constantly listen for when the user selects a
-   * a text in the Block Editor.
-   *
-   * @since 1.2.0
-   *
-   * @returns {void}
-   */
-  useEffect(() => {
-    document.addEventListener(
-      'selectionchange', onSelection
-    );
-
-    return () => {
-      document.removeEventListener(
-        'selectionchange', onSelection
-      );
-    };
-  }, []);
 
   /**
    * Handle case sensitive toggle feature
@@ -234,6 +189,28 @@ const SearchReplaceForBlockEditor = () => {
       setReplacements((items) => items + 1);
     }
   }
+
+  /**
+   * Listen for Selection.
+   *
+   * Constantly listen for when the user selects a
+   * a text in the Block Editor.
+   *
+   * @since 1.2.0
+   *
+   * @returns {void}
+   */
+  useEffect(() => {
+    document.addEventListener(
+      'selectionchange', handleSelection
+    );
+
+    return () => {
+      document.removeEventListener(
+        'selectionchange', handleSelection
+      );
+    };
+  }, []);
 
   return (
     <>
