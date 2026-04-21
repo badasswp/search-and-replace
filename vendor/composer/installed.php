708ff700314144165657028549acab150f294cda
badasswp <badasswpdev@gmail.com>
2026-04-21 09:15:43 +0000

    Build from release-1.10.1

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/search-replace-for-block-editor',
+        'pretty_version' => 'dev-release-1.10.1-built',
+        'version' => 'dev-release-1.10.1-built',
+        'reference' => '8d6bab2f1a00faad6eae248b50f28fcfb1d4ca78',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/search-replace-for-block-editor' => array(
+            'pretty_version' => 'dev-release-1.10.1-built',
+            'version' => 'dev-release-1.10.1-built',
+            'reference' => '8d6bab2f1a00faad6eae248b50f28fcfb1d4ca78',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
