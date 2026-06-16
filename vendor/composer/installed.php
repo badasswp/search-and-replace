5ea2178c280995165ea5cb64d54bfb45e0ecc9d8
badasswp <badasswpdev@gmail.com>
2026-06-16 12:32:42 +0000

    Build from release-1.11.0

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/search-replace-for-block-editor',
+        'pretty_version' => 'dev-release-1.11.0-built',
+        'version' => 'dev-release-1.11.0-built',
+        'reference' => 'aaa8b3ed605b26dfb46e557fdd3efc937bc59f4d',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/pluginate' => array(
+            'pretty_version' => 'v1.0.0',
+            'version' => '1.0.0.0',
+            'reference' => '2b28213dcc47ce0dc524e904a4bc8f51341fd5b5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../badasswp/pluginate',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'badasswp/search-replace-for-block-editor' => array(
+            'pretty_version' => 'dev-release-1.11.0-built',
+            'version' => 'dev-release-1.11.0-built',
+            'reference' => 'aaa8b3ed605b26dfb46e557fdd3efc937bc59f4d',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
