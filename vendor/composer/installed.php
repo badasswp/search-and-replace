c826d40ff67a9c81150a5147606e265e34f3b88f
badasswp <badasswpdev@gmail.com>
2026-06-28 17:17:53 +0000

    push to master-built

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/search-replace-for-block-editor',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '1ee922c983ef23116b8a09430447d94e29600026',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/pluginate' => array(
+            'pretty_version' => 'v1.0.0',
+            'version' => '1.0.0.0',
+            'reference' => '2b28213dcc47ce0dc524e904a4bc8f51341fd5b5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../badasswp/pluginate',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'badasswp/search-replace-for-block-editor' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '1ee922c983ef23116b8a09430447d94e29600026',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
