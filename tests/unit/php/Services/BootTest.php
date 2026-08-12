14ad1f11e218022302bd6a76a908b8a70ad9a1f9
badasswp <badasswpdev@gmail.com>
2026-08-12 01:11:08 +0100

    test: use levels parameter in place of nested dirname calls

--- tests/unit/php/Services/BootTest.php
+++ tests/unit/php/Services/BootTest.php
@@ -78,7 +78,7 @@ class BootTest extends WPMockTestCase {
 			->andReturn( '/var/www/wp-content/plugins/search-replace-for-block-editor/inc/Services/' );
 
 		WP_Mock::userFunction( 'plugins_url' )
-			->with( 'dist/app.js', dirname( dirname( $boot->getFileName() ) ) )
+			->with( 'dist/app.js', dirname( $boot->getFileName(), 2 ) )
 			->andReturn( 'https://example.com/wp-content/plugins/search-replace-for-block-editor/dist/app.js' );
 
 		WP_Mock::userFunction( 'wp_enqueue_script' )
